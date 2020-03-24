91711dda4cda2ad2d18ed721be2cc8568a617a01
Paulo Lucas Cerqueira <51492825+paulo-lucas@users.noreply.github.com>
2020-03-24 04:03:34 -0300

    URL de request do JSON alterada

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -3,7 +3,7 @@ var botaoAdicionar = document.querySelector("#buscar-pacientes");
 botaoAdicionar.addEventListener("click", function(){
     var xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "http://api-pacientes.herokuapp.com/pacientes");
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
     xhr.addEventListener("load", function(){
         var mensagemErro = document.querySelector("#erro-ajax");
         
